d8bc560bb897102f4e771f378010fc0595bb7db4
frochard <user1@148.60.14.89>
2016-04-06 21:16:13 +0200

    suppression import inutile

--- testjpa/src/main/java/fr/istic/sir/rest/HeaterService.java
+++ testjpa/src/main/java/fr/istic/sir/rest/HeaterService.java
@@ -12,8 +12,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import domain.ElectronicDevice;
 import domain.Heater;
 import jpa.EntityManagerHelper;
 
